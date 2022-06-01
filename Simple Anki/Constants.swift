a7b5dafdf565723dd9f323f4e44ccc779ccdae10
Astemir Boziev <bootuz07@gmail.com>
2022-06-02 00:37:41 +0400

    lint fixes

--- Simple Anki/Constants.swift
+++ Simple Anki/Constants.swift
@@ -11,13 +11,13 @@ import Foundation
 struct K {
     static let deckCellIdentifier = "decks"
     static let cardCellIdentifier = "cardCell"
-    
+
     struct Layout {
         static let frontToBack = "frontToBack"
         static let backToFront = "backToFront"
         static let all = "all"
     }
-    
+
     struct Settings {
         static let rateThisApp = "Rate this app"
         static let reportBug = "Report a bug"
@@ -25,12 +25,12 @@ struct K {
         static let darkMode = "Dark mode"
         static let shareThisApp = "Share this app"
         static let reminderOn = "Turn on"
-        
+
         static let support = "support"
         static let appearence = "appearence"
         static let notifications = "notifications"
     }
-    
+
     struct Icon {
         static let lefthalf = "circle.lefthalf.fill"
         static let ladybug = "ladybug"
@@ -39,14 +39,13 @@ struct K {
         static let bell = "bell.badge"
         static let share = "square.and.arrow.up"
     }
-    
+
     struct UserDefaultsKeys {
         static let reminder = "reminder"
         static let reminderTime = "reminderTime"
         static let darkMode = "dark_mode"
     }
-    
+
     static let email = "help@simpleanki.com"
     static let appURL = "https://apple.co/2Sx5VC1"
 }
-
