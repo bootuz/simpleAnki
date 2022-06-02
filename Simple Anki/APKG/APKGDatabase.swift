91223947665ff392f7d21b2fc74c6dcf0c95d419
Astemir Boziev <bootuz07@gmail.com>
2022-06-02 12:55:03 +0400

    returned apkg folder

--- Simple Anki/APKG/APKGDatabase.swift
+++ Simple Anki/APKG/APKGDatabase.swift
@@ -11,10 +11,10 @@ import SQLite
 
 
 enum SQLiteError: Error {
-  case OpenDatabase(message: String)
-  case Prepare(message: String)
-  case Step(message: String)
-  case Bind(message: String)
+    case OpenDatabase(message: String)
+    case Prepare(message: String)
+    case Step(message: String)
+    case Bind(message: String)
 }
 
 struct Queries {
