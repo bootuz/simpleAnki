d5aaa507cbf5b9dc6535f8fe243012e81f3920d3
Astemir Boziev <bootuz07@gmail.com>
2022-06-02 11:57:32 +0400

    lint fixes

--- Simple Anki/Managers/EmailManager.swift
+++ Simple Anki/Managers/EmailManager.swift
@@ -7,23 +7,22 @@
 
 import UIKit
 
-
 class EmailManager {
     
     private static var emailUrl: URL!
-
+    
     static func prepareEmailForBugReport() {
         let subject = "Bug report. App version: \(String(describing: UIApplication.version))".addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed)!
         prepareEmailUrl(with: subject)
         UIApplication.shared.open(emailUrl, options: [:], completionHandler: nil)
     }
-
+    
     static func prepareEmailForFeatureSuggestion() {
         let subject = "Feature request".addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed)!
         prepareEmailUrl(with: subject)
         UIApplication.shared.open(emailUrl, options: [:], completionHandler: nil)
     }
-
+    
     private static func prepareEmailUrl(with subject: String) {
         emailUrl = URL(string: "mailto:\(K.email)?subject=\(subject)")!
     }
